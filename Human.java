c0460ab71ab925e105bb61d87324c4e3e3f9c916
ParnianAghili <168179614+parnianAghili@users.noreply.github.com>
2024-05-17 12:30:06 +0330

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,21 @@
+package exercise1;
+
+public abstract class Human {
+    private String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void sayMyName() {
+        System.out.println("I'm a human!");
+    }
+
+    public final void staticPrint() {
+        System.out.println("This function should always print this string in all subclasses");
+    }
+}
