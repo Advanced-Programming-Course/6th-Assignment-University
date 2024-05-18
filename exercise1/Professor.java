91321e9fb0152a99d5f0682594ae6a08bb76eff9
YasaminMirabdolah <yasminmirabdolah@gmail.com>
2024-05-18 12:58:43 -0700

    Q4

--- exercise1/Professor.java
+++ exercise1/Professor.java
@@ -5,4 +5,31 @@ package exercise1;
 //  5.Change the professor class so that when we call the sayMyName() method on an instance of this class, fullName of the professor plus their professorFaculty is printed.
  
 public class Professor {
+    public class Professor extends Human{
+        // سوال 4
+        private String professorFaculty;
+        private String professorSpecialty;
+        private int numberOfCourse;
+
+        public String getProfessorFaculty(){
+            return professorFaculty;
+        }
+        public void setProfessorFaculty(String professorFaculty){
+            this.professorFaculty=professorFaculty;
+        }
+        public String getProfessorSpecialty(){
+            return professorSpecialty;
+        }
+        public void setProfessorSpecialty(String professorSpecialty){
+            this.professorSpecialty=professorSpecialty;
+        }
+        public int getNumberOfCourse(){
+            return numberOfCourse;
+        }
+        public void setNumberOfCourse(int numberOfCourse){
+            this.numberOfCourse=numberOfCourse;
+        }
+
+    }
+
 }
