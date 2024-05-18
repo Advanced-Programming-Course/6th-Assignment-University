ed28bda9f4b51751886f84f4b1cbaef8a040a0b5
Mahsa <mahsamousavi1384@gmail.com>
2024-05-18 20:30:29 +0330

    commit 1

--- exercise1/Human.java
+++ exercise1/Human.java
@@ -3,7 +3,7 @@ package exercise1;
 // 1.Change the staticPrint() method so that its output cannot be changed in all the classes that extend or implement Human.
 
 public abstract class Human {
-    private String fullName;
+    public String fullName;
 
     public String getFullName() {
         return fullName;
@@ -17,7 +17,7 @@ public abstract class Human {
         System.out.println("im a human!");
     }
 
-    public void staticPrint() {
+    public static void staticPrint() {
         System.out.println("this function should always print this string in all subclasses");
     }
 }
--- exercise1/Student.java
+++ exercise1/Student.java
@@ -5,5 +5,36 @@ package exercise1;
 // 2. Change the student class so that when we call the sayMyName() method on an instance of this class, the fullName attribute of the student is printed.
 // 3.Add the following attributes to the student class with setters and getters: studentNumber, majorName, universityName
 
-public class Student {
+public class Student extends Human {
+    private String fullName;
+    private String studentNumber;
+    private String majorName;
+    private String universityName;
+
+    public Student(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+    public String getMajorName() {
+        return majorName;
+    }
+    public String getUniversityName() {
+        return universityName;
+    }
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+    public void setMajorName(String majorName) {
+        this.majorName = majorName;
+    }
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public void sayMyName() {
+        System.out.println(fullName);
+    }
 }
+
