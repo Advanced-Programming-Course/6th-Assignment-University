06f4461bb93aafc59f2834002b0951efe24ce7d6
mohadesemoradzade <mohadesemoradzade3@gmail.com>
2024-05-18 22:47:44 +0330

    finish

--- exercise1/Main.java
+++ exercise1/Main.java
@@ -1,7 +1,5 @@
 package exercise1;
 
-import java.util.ArrayList;
-import java.util.List;
 
 // After completing the Student and Professor classes, create an instance of each one:
 //  6. Using instanceof keyword, check if the instances that you have created are really an instance of human class
@@ -13,6 +11,32 @@ import java.util.List;
  
 public class Main {
     public static void main(String[] args) {
-
+        //Question6
+        Student student = new Student("John Doe");
+        Professor professor = new Professor("Dr. Smith");
+        if (student instanceof Human) {
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+        if (professor instanceof Human) {
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+        //Question7
+        /*
+        Human human = new Student("whatever");
+        human.sayMyName();
+        */
+        //Question8
+        /*
+        Human human = new Professor("whatever");
+        human.sayMyName();
+        */
+        //Question9
+        //Taghiri dar yek class tasiri bar digari nadarad.
     }
 }
--- exercise1/Professor.java
+++ exercise1/Professor.java
@@ -4,5 +4,33 @@ package exercise1;
 //  4.Add the following attributes to this class with setters and getters: professorSpecialty, professorFaculty, numberOfCourse
 //  5.Change the professor class so that when we call the sayMyName() method on an instance of this class, fullName of the professor plus their professorFaculty is printed.
  
-public class Professor {
+public class Professor extends Human {
+    private String professorFaculty;
+    private String numberOfCourse;
+    private String professorSpecialty;
+
+    public Professor(String fullName){
+    }
+
+    public String getProfessorFaculty() {
+        return professorFaculty;
+    }
+    public String getNumberOfCourse() {
+        return numberOfCourse;
+    }
+    public String getProfessorSpecialty() {
+        return professorSpecialty;
+    }
+    public void setProfessorFaculty(String professorFaculty) {
+        this.professorFaculty = professorFaculty;
+    }
+    public void setNumberOfCourse(String numberOfCourse) {
+        this.numberOfCourse = numberOfCourse;
+    }
+    public void setProfessorSpecialty(String professorSpecialty) {
+        this.professorSpecialty = professorSpecialty;
+    }
+    public void sayMyName() {
+        System.out.print(fullName + professorFaculty);
+    }
 }
